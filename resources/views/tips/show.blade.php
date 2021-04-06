dd3a3a3553bd406bc820d8dac3e8ee33c89e25fc
Samuel Štancl <samuel.stancl@gmail.com>
2021-04-07 01:02:39 +0200

    use title instead of content for og image

--- resources/views/tips/show.blade.php
+++ resources/views/tips/show.blade.php
@@ -1,4 +1,4 @@
-<x:layout :title="$tip->title" :preview="$tip->content">
+<x:layout :title="$tip->title" :preview="$tip->title">
     <main class="mx-auto flex justify-between items-center gap-8">
         <aside class="flex flex-col gap-2">
             <h2 class="text-2xl font-medium">
